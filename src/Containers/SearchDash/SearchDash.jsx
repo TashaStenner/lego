98a927b19674fe3f00effbf64882e0db58e06d99
Tasha Stenner <tashas1997@gmail.com>
2020-06-23 15:46:13 +0100

    api-works

--- src/Containers/SearchDash/SearchDash.jsx
+++ src/Containers/SearchDash/SearchDash.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React , {useState} from "react";
 import styles from "./SearchDash.module.scss";
 import Card from "../../Components/Card";
 
 const SearchDash = (props) => {
   const { handleClick } = props;
 
+  const [number, setNumber] = useState();
+  const [name, setName] = useState();
+  const [year, setYear] = useState();
+  const [parts, setParts] = useState();
+  const [setImg, setSetImg] = useState();
+
   const getSets = () => {
     fetch(
       "https://rebrickable.com/api/v3/lego/sets/7633-1?key=b3467dbaf7b933bcef61dd2d62d6dc64"
     )
       .then((result) => result.json())
-      .then((result) => console.log(result))
+      .then((result) => (
+      setNumber(result.set_num),
+      setName(result.name),
+      setYear(result.year), 
+      setParts(result.num_parts),
+      setSetImg(result.set_img_url)
+      )   
+      )
       .catch((err) => console.log(err));
   };
 
+
   return (
     <>
-      <Card setNum={"7893"} setName={"name"} year={"1998"} parts={"238908"} imgUrl={"https://cdn.rebrickable.com/media/sets/7633-1/6688.jpg"}/>
+      <Card setNum={number} setName={name} year={year} parts={parts} imgUrl={setImg}/>
     </>
   );
 };

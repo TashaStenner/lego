545d57da49350d7778083b54873db7d599d35ad8
Tasha Stenner <tashas1997@gmail.com>
2020-07-06 13:08:40 +0100

    added-data-file

--- src/Components/InformationBox/InformationBox.jsx
+++ src/Components/InformationBox/InformationBox.jsx
@@ -5,9 +5,9 @@ const InformationBox = () => {
   return (
     <>
       <section className={styles.info}>
-        <div>
+        <div className={styles.data}>
           <p></p>
-          <p></p>
+          <p className={styles.referenceLink}><a href="https://kidskonnect.com/fun/lego/">LEGO Facts & Worksheets: https://kidskonnect.com</a> - KidsKonnect, December 15, 2016</p>
         </div>
       </section>
     </>
---
+++ src/assets/data/facts.js
@@ -0,0 +1,22 @@
+let funFacts = 
+[
+    "LEGO DUPLO bricks are eight times the size of original LEGO bricks. Because of the universal system they will still connect together.",
+    "Each LEGO minifigure is exactly four bricks high without a hat.",
+    "In 2009, British TV presenter James May built a life size house out of LEGO. It used 3.3 million bricks and it included a working toilet, a shower and a bed.",
+    "Seven LEGO sets are sold every second.", 
+    "LEGO was originally called “Automatic Binding Bricks”.",
+    "A LEGO brick from 1958 would interlock with a brick from 2015", 
+    "You could reach the moon with a column of around 40 billion LEGO bricks. The column would be over 384,400 km tall",
+    "Six eight-stud LEGO bricks can be combined in over 915 million different ways.",
+    "In 2000, LEGO was named “Toy of the Century” by the British Association of Toy Retailers. LEGO beat both the common teddy bear and the Barbie doll.",
+    "In 2011, astronauts took LEGO to space. To see how they would react in microgravity",
+    "There are no LEGO sets with a war or military theme",
+    "The largest commercial LEGO set is the Taj Mahal set. It has 5,922 individual pieces.",
+    "The plural of LEGO is LEGO.", 
+    "LEGO comes from the Danish word leg godt. Leg godt means “play well” in Danish and LEGO is made from the first two letters of each word.", 
+    "The LEGO Group began in 1932 in Billund, Denmark and was founded by Ole Kirk Christiansen.",
+    "18 out of every million LEGO pieces fail to meet the company standard.",
+    "If you laid all of the LEGO bricks sold in 2012 end-to-end, they would stretch around the world more than 18 times."
+]
+
+//Facts were found on https://kidskonnect.com/fun/lego/
